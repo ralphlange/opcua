32b8d9429815ef961a02b222ccac1d7b3d8b71c9
Ralph Lange <ralph.lange@gmx.de>
2026-09-11 12:58:31 +0200

    ci: use generic URL and more retries for downloading from Ubuntu archives

--- .ci-local/install-sdk.py
+++ .ci-local/install-sdk.py
@@ -11,7 +11,7 @@ import re
 
 curdir = os.getcwd()
 openssl_debs = ['libssl1.1_1.1.1f-1ubuntu2.24_amd64.deb', 'libssl-dev_1.1.1f-1ubuntu2.24_amd64.deb']
-openssl_url  = 'http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl'
+openssl_url  = 'http://archive.ubuntu.com/ubuntu/pool/main/o/openssl'
 
 sys.path.append(os.path.join(curdir, '.ci'))
 import cue
@@ -80,7 +80,7 @@ OPENSSL = $(UASDK)/third-party/win64/vs2015/openssl''')
     # Downgrade libssl to 1.1.1 on GHA's Ubuntu 22.04 and up
     if cue.ci['service'] == 'github-actions' and cue.ci['os'] == 'linux':
         for deb in openssl_debs:
-            sp.check_call(['curl', '-fsSLO', '--retry', '3', '{0}/{1}'.format(openssl_url, deb)],
+            sp.check_call(['curl', '-fsSLO', '--retry', '5', '--retry-connrefused', '{0}/{1}'.format(openssl_url, deb)],
                           cwd=cue.toolsdir)
             sp.check_call(['sudo', 'dpkg', '-i', deb],
                           cwd=cue.toolsdir)
